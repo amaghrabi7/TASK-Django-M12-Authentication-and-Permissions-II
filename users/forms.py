1961d3ac0d1c18f1a2272d5d69cdeb98cdd47217
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-19 20:30:50 +0300

    finished registration section

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+# from socket import fromshare
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserRegister(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "password"]
+
+        widgets = {
+            "password": forms.PasswordInput(),
+        }
+
---
+++ users/views.py
@@ -0,0 +1,24 @@
+from pdb import post_mortem
+from django.shortcuts import render, redirect
+from users.forms import UserRegister
+from django.contrib.auth import login
+
+# Create your views here.
+
+def user_register(request):
+    form = UserRegister()
+    if request.method == "POST":
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            user.set_password(user.password)
+            user.save()
+
+            login(request, user)
+            return redirect("home")
+    context = {
+        "form": form,
+    }
+
+    return render(request, "register.html", context)
